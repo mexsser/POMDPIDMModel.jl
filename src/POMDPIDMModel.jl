78adb01cd2f41ec56a15ae65cc06d656d05eff8c
mexsser <mexsser@gmail.com>
2019-03-08 14:52:18 +0100

    Files generated by PkgTemplates

---
+++ src/POMDPIDMModel.jl
@@ -0,0 +1,5 @@
+module POMDPIDMModel
+
+greet() = print("Hello World!")
+
+end # module
---
+++ test/runtests.jl
@@ -0,0 +1,6 @@
+using POMDPIDMModel
+using Test
+
+@testset "POMDPIDMModel.jl" begin
+    # Write your own tests here.
+end
