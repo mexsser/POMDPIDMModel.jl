2c7986a4e83d9b7da1789c8adc818f063e5307a4
mexsser <mexsser@gmail.com>
2019-03-11 13:22:05 +0100

    add deps

--- deps/build.jl
+++ deps/build.jl
@@ -4,19 +4,21 @@ using Pkg
 Pkg.add("POMDPs")
 import POMDPs
 POMDPs.add_registry()
-Pkg.add("QMDP")
 
 Pkg.add("Random")
+Pkg.add("StatsBase")
 Pkg.add("Distributions")
 Pkg.add("LinearAlgebra")
 Pkg.add("SymPy")
 
 Pkg.add("POMDPModelTools")
+Pkg.add("POMDPPolicies")
+Pkg.add("POMDPSimulators")
 Pkg.add("BeliefUpdaters")
-Pkg.add(PackageSpec(url = "https://github.com/mexsser/Geo.jl"))
 Pkg.add(PackageSpec(url = "https://github.com/mexsser/DiscreteValueIteration.jl"))
+Pkg.add("QMDP")
 
+Pkg.add(PackageSpec(url = "https://github.com/mexsser/Geo.jl"))
 Pkg.add("Plots")
-Pkg.add("StatsBase")
 Pkg.add("Distributed")
 Pkg.add("Measures")
